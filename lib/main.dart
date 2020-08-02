ea800da58fdb1ff3925dd0de5c2cb774163b6611
AldoReghan <aldoreghan2217@gmail.com>
2020-08-03 00:17:45 +0700

    designku

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter/services.dart';
+import 'package:flip_card/flip_card.dart';
 
 void main() {
   runApp(MyApp());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.dark));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(canvasColor: Colors.black),
       home: Home(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -44,67 +51,203 @@ class _HomeState extends State<Home> {
         ],
       ),
       drawer: Drawer(
-        child: Center(
-          child: Text("Hello world"),
-        ),
-      ),
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            height: 210,
+            child: DrawerHeader(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Aldo Reghan Ramadhan",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "email@gmail.com",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            )),
+            color: Colors.blue,
+          ),
+          ListTile(
+            title: Text(
+              "Home",
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              "Karya",
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: Icon(
+              Icons.image,
+              color: Colors.white,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              "Transaksi",
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: Icon(
+              Icons.compare_arrows,
+              color: Colors.white,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              "Favorit",
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: Icon(
+              Icons.favorite,
+              color: Colors.white,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              "Settings",
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+            child: Divider(
+              color: Colors.blue,
+              thickness: 0.5,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              "Login",
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: Icon(
+              Icons.account_circle,
+              color: Colors.white,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              "Logout",
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: Icon(
+              Icons.exit_to_app,
+              color: Colors.white,
+            ),
+          )
+        ],
+      )),
       body: SizedBox.expand(
         child: Stack(
           alignment: Alignment.center,
           overflow: Overflow.visible,
           children: <Widget>[
-            // Positioned(
-            //   top: 30,
-            //   left: 20,
-            //   right: 20,
-            //   child: Row(
-            //     children: <Widget>[
-            //       Icon(
-            //         Icons.menu,
-            //         color: Colors.white,
-            //       ),
-            //       Spacer(),
-            //       Text(
-            //         "Designku",
-            //         textAlign: TextAlign.center,
-            //         style: TextStyle(
-            //             color: Colors.white,
-            //             fontSize: 35,
-            //             fontFamily: 'Sacramento'),
-            //       ),
-            //       Spacer(),
-            //       Icon(
-            //         Icons.shopping_cart,
-            //         color: Colors.white,
-            //       )
-            //     ],
-            //   ),
-            // ),
             Positioned(
               top: 20,
               child: Container(
-                height: 450,
+                height: 460,
                 width: MediaQuery.of(context).size.width,
                 child: Swiper(
                   itemBuilder: (context, index) {
                     return Container(
-                      height: MediaQuery.of(context).size.height,
-                      decoration: BoxDecoration(
+                        // height: MediaQuery.of(context).size.height,
+                        decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
-                          color: Colors.grey),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.network(
-                          "http://via.placeholder.com/288x188",
-                          fit: BoxFit.fill,
                         ),
-                      ),
-                    );
+                        child: FlipCard(
+                            front: GestureDetector(
+                              onDoubleTap: () => print("object"),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Image.network(
+                                  "http://via.placeholder.com/288x188",
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                            back: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    border: Border.all(color: Colors.white)),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Row(
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              "The dark is rising",
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                  fontSize: 20,
+                                                  fontFamily: 'Pacifico',
+                                                  color: Colors.white),
+                                            ),
+                                          ),
+                                          Spacer(),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Icon(
+                                              Icons.favorite, color: Colors.red, size: 30,
+                                            )
+                                          ),
+                                        ],
+                                      ),
+                                      Divider(
+                                        thickness: 0.5,
+                                        color: Colors.blue,
+                                      ),
+                                      Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Container(
+                                              height: 300,
+                                              color: Colors.white,
+                                              child: ListView())),
+                                      Padding(
+                                        padding: EdgeInsets.all(8),
+                                        child: Row(
+                                          children: <Widget>[
+                                            
+                                          ]
+                                        )
+                                      )
+                                    ],
+                                  ),
+                                ))));
                   },
                   itemCount: 3,
                   viewportFraction: 0.8,
                   scale: 0.9,
-                  autoplay: true,
+                  // autoplay: true,
                   itemWidth: MediaQuery.of(context).size.width,
                   itemHeight: MediaQuery.of(context).size.height,
                 ),
@@ -117,7 +260,8 @@ class _HomeState extends State<Home> {
                   maxChildSize: 0.5,
                   builder: (BuildContext c, s) {
                     return Container(
-                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 5, vertical: 20),
                       decoration: BoxDecoration(
                           color: Colors.black,
                           borderRadius: BorderRadius.only(
@@ -173,7 +317,7 @@ class _HomeState extends State<Home> {
                               style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 20,
-                                  fontFamily: 'Pacifio'),
+                                  fontFamily: 'Pacifico'),
                             ),
                           ),
                           SizedBox(
